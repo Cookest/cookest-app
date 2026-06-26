ba7185b168aa6fccc00b823576a62b140157a262
Tiago Oliveira <tiagoco2008@gmail.com>
2026-06-26 23:15:30 +0100

    whatever

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:cookest_ui/cookest_ui.dart';
 
 void main() {
-  testWidgets('Smoke test', (WidgetTester tester) async {
-    // Tests will be added later
-    expect(true, isTrue);
+  testWidgets('CkButton layout constraints test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: SizedBox(
+              width: 127,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CkButton(
+                      onPressed: () {},
+                      child: const Text('Very long text that will overflow when width is constrained to 127'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byType(CkButton), findsOneWidget);
   });
 }
